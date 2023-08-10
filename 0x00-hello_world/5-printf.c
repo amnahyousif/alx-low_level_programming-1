adf6fc323113829c8783f20ce0182953df16ce51
amnaha1 <tofahamoon94@gmail.com>
2023-08-10 15:44:30 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+/*
+ * main: entry point 
+ * description:a C program that print with printf function
+ * return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);	
+}
