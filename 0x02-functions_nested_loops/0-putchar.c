db7a4217af147a1e44700592553ee6d3b6a43761
amnaha1 <tofahamoon94@gmail.com>
2023-08-16 08:13:21 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ * using putchar prototype 
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char str[] = "_putchar";
+int ch;
+for (ch = 0; ch < 8; ch++)
+_putchar(std[ch]);
+_putchar('\n');
+return (0);
+}
