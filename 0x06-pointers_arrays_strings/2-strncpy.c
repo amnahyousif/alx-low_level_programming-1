f8e00337113d09295464807ad7ef9c00be63047c
amnaha1 <tofahamoon94@gmail.com>
2023-08-24 05:13:19 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+/**
+ * print_number - print an int numbers.
+ * @n: number tested
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+int i, j, digit, digits, power;
+unsigned int a, b, c;
+
+digit = 0;
+if (n < 0)
+{
+_putchar('-');
+a = -n;
+}
+else
+{
+a = n;
+}
+c = a;
+
+while (c >= 10)
+{
+c = c / 10;
+digit++;
+}
+digits = digit + 1;
+power = 1;
+i = 1;
+
+while (i < digits)
+{
+power = power * 10;
+i++;
+}
+j = power;
+while (j >= 1)
+{
+b = (a / j) % 10;
+_putchar(b + '0');
+j = j / 10;
+}
+}
