c31e7088459a804ac33260648a2bdc684711eb13
amnaha1 <tofahamoon94@gmail.com>
2023-08-24 05:51:01 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,45 +1,18 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * print_number - print an int numbers.
- * @n: number tested
+ * _strncpy - concatenate strings, defining the size of second string.
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * @n: size of second string
  * Return: Always 0.
  */
-void print_number(int n)
+char *_strncpy(char *dest, char *src, int n)
 {
-int i, j, digit, digits, power;
-unsigned int a, b, c;
+int i;
 
-digit = 0;
-if (n < 0)
-{
-_putchar('-');
-a = -n;
-}
-else
-{
-a = n;
-}
-c = a;
-
-while (c >= 10)
-{
-c = c / 10;
-digit++;
-}
-digits = digit + 1;
-power = 1;
-i = 1;
-
-while (i < digits)
-{
-power = power * 10;
-i++;
-}
-j = power;
-while (j >= 1)
-{
-b = (a / j) % 10;
-_putchar(b + '0');
-j = j / 10;
-}
+for (i = 0; i < n && src[i] != '\0'; i++)
+*(dest + i) = *(src + i);
+for ( ; i < n; i++)
+*(dest + i) = '\0';
+return (dest);
 }
