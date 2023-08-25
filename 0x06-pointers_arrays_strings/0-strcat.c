07d8338ae9172965ef1e98ed3baf39f115e9fdff
amnaha1 <tofahamoon94@gmail.com>
2023-08-24 21:50:56 -0700

    an edit on readme file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,20 +7,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int a, b;
+	int a, b;
 
-a = 0;
-b = 0;
+	a = 0;
+	b = 0;
 
-while (*(dest + a) != '\0')
-a++;
+	while (*(dest + a) != '\0')
+		a++;
 
-while (*(src + b) != '\0' && a < 97)
-{
-*(dest + a) = *(src + b);
-a++;
-b++;
-}
-*(dest + a) = '\0';
-return (dest);
+	while (*(src + b) != '\0' && a < 97)
+	{
+		*(dest + a) = *(src + b);
+		a++;
+		b++;
+	}
+	*(dest + a) = '\0';
+	return (dest);
 }
