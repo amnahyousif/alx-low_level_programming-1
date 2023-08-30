ebc60bcdc1a9c92eabd773ccc76c189fa29bdb87
amnaha1 <tofahamoon94@gmail.com>
2023-08-30 04:48:35 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - Write a function that prints a string, followed by a new
+ * line.
+ *
+ * @s: This is the input string
+ */
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+_putchar('\n');
+}
+_putchar(*s);
+_puts_recursion(s + 1);
+}
