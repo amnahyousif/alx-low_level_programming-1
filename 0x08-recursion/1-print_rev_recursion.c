3fe89753d7428ad16a9f2210f9d3f85dc3df8a46
amnaha1 <tofahamoon94@gmail.com>
2023-08-30 05:54:13 -0700

    2. Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-*_print_rev_recursion - Write a function that prints a string in reverse.
-*@s: point to start 
-*Return: void 
-*/
+ * _print_rev_recursion - Write a function that prints a string in reverse.
+ * @s: point to start
+ * Return: viod. 
+ */
 void _print_rev_recursion(char *s)
 {
 if (*s)
